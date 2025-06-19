bf6d45bc1e15db1da513e14b33711771f522f924
comcraftdev <84923453+comcraftdev@users.noreply.github.com>
2025-06-20 00:57:46 +0200

    world set bound check

--- src/comcrafthd/ChunkWorld.java
+++ src/comcrafthd/ChunkWorld.java
@@ -5,6 +5,10 @@ public final class ChunkWorld {
     private final ChunkList chunkList = ComcraftGame.instance.chunkList;
 
     public void set(final int blockX, final int blockY, final int blockZ, final short value) {
+        if (blockY < 0 || blockY >= Chunk.CHUNK_HEIGHT) {
+            return;
+        }
+        
         final int chunkX = blockX >> Chunk.BLOCK_TO_CHUNK_SHIFT;
         final int chunkZ = blockZ >> Chunk.BLOCK_TO_CHUNK_SHIFT;
 
